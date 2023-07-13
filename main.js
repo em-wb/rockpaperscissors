5f04be610d00f0a3d2100672bd1628e68ebc7c04
em-wb <131539794+em-wb@users.noreply.github.com>
2023-07-13 10:49:07 +0100

    Add variables for counter. Add prompt for user input, make response case insensitive. Add for loop for 5 game rounds. Add console log for game score counter.

--- main.js
+++ main.js
@@ -6,30 +6,67 @@ function getComputerChoice() {
     return randomPick;
   }
 
+
 // Make variables for player and computer selection
 
 const computerSelection = getComputerChoice();
-console.log(computerSelection);
-const playerSelection = "scissors";
+    console.log(computerSelection);
+const playerSelection = "Scissors";
+const rounds = 4;
 
-// Specify game scenarios 
 
-  function playRound(playerSelection, computerSelection) {
-        if (playerSelection === "rock" && computerSelection === "scissors") {
+//UPDATE: make variables for counting score 
+
+const playerScore = 0
+const computerScore = 0
+const tieScore = 0 
+
+
+// Specify game scenarios // UPDATE: make lower case // UPDATE: add prompt for user
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = prompt("What do you choose? Rock, paper or scissors!") 
+        if (playerSelection.toLowerCase() === "rock" && computerSelection === "scissors") {
             return ("Rock blunts scissors, you win!")
-        } else if (playerSelection === "rock" && computerSelection === "paper") {
+            
+        } else if (playerSelection.toLowerCase() === "rock" && computerSelection === "paper") {
             return ("Paper wraps rock, you lose!")
-        } else if (playerSelection === "paper" && computerSelection === "scissors") {
+            
+        } else if (playerSelection.toLowerCase() === "paper" && computerSelection === "scissors") {
             return ("Scissors cut paper, you lose!")
-        } else if (playerSelection === "paper" && computerSelection === "rock") {
+            
+        } else if (playerSelection.toLowerCase() === "paper" && computerSelection === "rock") {
             return ("Paper wraps rock, you win!")
-        } else if (playerSelection === "scissors" && computerSelection === "rock") {
+            
+        } else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "rock") {
             return ("Rock blunts scissors, you lose!")   
-        } else if (playerSelection === "scissors" && computerSelection === "paper") {
+           
+        } else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "paper") {
             return ("Scissors cut paper, you win!") 
-        } else if (playerSelection === computerSelection) {
+           
+        } else if (playerSelection.toLowerCase() === computerSelection) {
             return ("It's a draw!")
+            
         }
-    }
-       
-      console.log (playRound(playerSelection, computerSelection));
+}
+
+console.log(playRound(playerSelection, computerSelection));
+
+// UPDATE: write a function that counts 5 rounds (at the moment return message doesn't update)
+
+for (let i = 0; i < rounds; i++) {
+    playRound() 
+}
+
+
+//UPDATE add view for counter  (at the moment, counting not set up)
+
+console.log(playerScore, computerScore, tieScore)
+
+
+
+
+
+    
+
+  
