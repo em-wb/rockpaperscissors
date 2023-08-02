fa6d366c335b072bbb63ae08c30db0e95b01d2e2
em-wb <131539794+em-wb@users.noreply.github.com>
2023-08-02 09:52:39 +0100

    Build game UI

--- main.js
+++ main.js
@@ -1,77 +1,122 @@
-// Make the computer select either Rock, Paper or Scissors randomly 
+// Make the computer randomly select rock, paper or scissors 
 function getComputerChoice() {
     let options = ["rock", "paper", "scissors"];
     let randomPick = options[Math.floor(Math.random() * options.length)];
     return randomPick;
   }
 
-// Add variables for player selection, number of rounds
-const playerSelection = "Scissors";
-const rounds = 5;
-
 // Add variables for counting score
 let playerScore = 0
 let computerScore = 0
 let tieScore = 0 
+let count = 0
+let rounds = 5
 
+// Create game. Specify rules and display outcomes.
 
-// Specify game scenarios, prompt user to make their selection and make their choice case insensitive
 function game() {
     const computerSelection = getComputerChoice();  
     function playRound(playerSelection, computerSelection) {
-        playerSelection = prompt("What do you choose? Rock, paper or scissors!") 
-            if (playerSelection.toLowerCase() === "rock" && computerSelection === "scissors") {
+            if (playerSelection === "rock" && computerSelection === "scissors") {
                 playerScore++
-                return ("Rock blunts scissors, you win!")  
-            } else if (playerSelection.toLowerCase() === "rock" && computerSelection === "paper") {
+                //return ("Rock blunts scissors, you win!")
+                textResult.textContent = "Yaaas, rock blunts scissors."
+            } else if (playerSelection === "rock" && computerSelection === "paper") {
                 computerScore++
-                return ("Paper wraps rock, you lose!")
-            } else if (playerSelection.toLowerCase() === "paper" && computerSelection === "scissors") {
+                //return ("Paper wraps rock, you lose!")
+                textResult.textContent = "Soz babe, paper wraps rock."
+            } else if (playerSelection === "paper" && computerSelection === "scissors") {
                 computerScore++
-                return ("Scissors cut paper, you lose!")   
-            } else if (playerSelection.toLowerCase() === "paper" && computerSelection === "rock") {
+                // return ("Scissors cut paper, you lose!") 
+                textResult.textContent = "Awww no, scissors cut paper."
+            } else if (playerSelection === "paper" && computerSelection === "rock") {
                 playerScore++
-                return ("Paper wraps rock, you win!")   
-            } else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "rock") {
+                // return ("Paper wraps rock, you win!")   
+                textResult.textContent = "Oi oi, paper wraps rock."
+            } else if (playerSelection === "scissors" && computerSelection === "rock") {
                 computerScore++   
-                return ("Rock blunts scissors, you lose!")
-            } else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "paper") {
+                //return ("Rock blunts scissors, you lose!")
+                textResult.textContent = "Oh snap, rock blunts scissors."
+            } else if (playerSelection === "scissors" && computerSelection === "paper") {
                 playerScore++ 
-                return ("Scissors cut paper, you win!")
-            } else if (playerSelection.toLowerCase() === computerSelection) {
-                tieScore++
-                return ("It's a draw!")    
-            } else {alert("Try again, by entering: rock, paper or scissors.");
-                game();
-                return ("NOT COUNTED, incorrect input :)")             
-                 } 
+                //return ("Scissors cut paper, you win!")
+                textResult.textContent = "Looking sharp, scissors cut paper."
+            } else if (playerSelection === computerSelection) {
+                tieScore++;
+                //return ("It's a draw!")    
+                textResult.textContent = "No drama, it's a draw."
+            }
+    }
+    console.log(playRound(playerSelection, computerSelection))
+    displayPlayerScore.textContent = playerScore;
+    displayComputerScore.textContent = computerScore;    
+    displayComputerSelection.textContent = ["The bot chose: " + computerSelection];  
+}
+
+// Restart game
+const restarts = document.querySelector("#restarts");
+
+restarts.addEventListener("click", (e) => {
+    count = 0;
+    playerScore=0;
+    computerScore=0;
+    endGameResult.textContent = "";
+    displayComputerSelection.textContent = "";
+    displayPlayerScore.textContent = "";
+    displayComputerScore.textContent = ""; 
+    textResult.textContent = ""; 
+})
+
+
+
+//Add event listener for button selection and commence gameplay for 5 rounds
+
+const possibleChoices = document.querySelectorAll("#buttonsContainer");
+let playerSelection;
+
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener("click", (e) => {
+    playerSelection = e.target.id;
+    count ++
+    if (count<rounds) {
+        game() }
+    else {
+        endGame()}
         
-    }
-        console.log("The computer chose " + computerSelection,"-", playRound(playerSelection, computerSelection))
-         // view game outcome
-        console.log("You've won: ", playerScore, ", lost: ", computerScore, ", tied: ", tieScore); // view score counter 
-    };
-    
+}))
+
+//Display winner of the 5 rounds
+function endGame() {
+    if (playerScore<computerScore) endGameResult.textContent = "After 5 rounds, the winner is... The bot :(";
+    if (playerScore>computerScore) endGameResult.textContent = "After 5 rounds, the winner is... You!";
+    else if (playerScore===computerScore) endGameResult.textContent = "After 5 rounds, the winner is... No one, it's a draw"
+}
 
 
-// A for loop that counts 5 playRounds 
-for (let i = 0; i < rounds; i++) {
-    game() } 
+//Add divs to display results
 
-// Report the overall winner of the game
-function gameOutcome(playerScore, computerScore) {
-    if (playerScore < computerScore) {
-        return ("After 5 rounds, you lose! Better luck next time.")
-    } else if (playerScore > computerScore) {
-        return ("After 5 rounds, you are the overall winner! Congratulations.")
-    } else {return ("After 5 rounds, it's a draw!")}
-    }
-
-    console.log (gameOutcome(playerScore, computerScore))
-
-    
+const buttonsContainer = document.querySelector('#buttonsContainer');
+const resultsContainer = document.querySelector('#resultsContainer');
+const textResult = document.querySelector('#textResult');
+const yourScore = document.querySelector('#yourScore');
+const botScore = document.querySelector('#botScore');
+const botChoice = document.querySelector('#botChoice');
+const endGameContainer = document.querySelector("#endGameContainer");
 
 
-    
+const displayPlayerScore = document.createElement('div');
+displayPlayerScore.classList.add('displayPlayerScore');
+displayPlayerScore.textContent = playerScore;
+yourScore.append(displayPlayerScore);
 
-  
+const displayComputerScore = document.createElement('div');
+displayComputerScore.classList.add('displayComputerScore');
+displayComputerScore.textContent = computerScore;
+botScore.append(displayComputerScore);
+
+const displayComputerSelection = document.createElement('div');
+displayComputerSelection.textContent = "";
+botChoice.append(displayComputerSelection);
+
+const endGameResult = document.createElement('div');
+endGameResult.textContent = "";
+endGameContainer.append(endGameResult);
